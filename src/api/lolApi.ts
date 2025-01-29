42b5b713c6d4752fac22438cc439f59c78d97902
Dani Derbala <Dani.derbala77210@gmail.com>
2025-01-29 10:23:29 +0100

    modification lolApi

--- src/api/lolApi.ts
+++ src/api/lolApi.ts
@@ -4,10 +4,10 @@ const BASE_URL = "https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US";
 
 export const fetchChampions = async () => {
   const response = await axios.get(`${BASE_URL}/champion.json`);
-  return response.data.data; // Liste des champions
+  return response.data.data;
 };
 
 export const fetchChampionById = async (id: string) => {
   const response = await axios.get(`${BASE_URL}/champion/${id}.json`);
-  return response.data.data[id]; // Détails du champion
+  return response.data.data[id];
 };
