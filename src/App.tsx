ac2d81245287d24be3e25fabd4b0cd1b9657c74e
Dani Derbala <Dani.derbala77210@gmail.com>
2025-01-31 13:24:38 +0100

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import NavBar from "./components/Navbar/NavBar";
-import HomePage from "./pages/HomePage/HomePage";
-import TierListPage from "./pages/TierListPage/TierListPage";
-import ChampionPage from "./pages/ChampionPage/ChampionPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/Navbar/NavBar';
+import HomePage from './pages/HomePage/HomePage';
+import ChampionPage from './pages/ChampionPage/ChampionPage';
+import TierListPage from './pages/TierListPage/TierListPage';
 
 function App() {
   return (
-    <>
-    <NavBar />
-    <Routes>
-      <Route path="/lol-companion/" element={<HomePage />} />
-      <Route path="/lol-companion/champion/:id" element={<ChampionPage />} />
-      <Route path="/lol-companion/tierlist" element={<TierListPage />} />
-    </Routes>
-    </>
+    <BrowserRouter basename="/lol-companion">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/champion/:id" element={<ChampionPage />} />
+        <Route path="/tierlist" element={<TierListPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
